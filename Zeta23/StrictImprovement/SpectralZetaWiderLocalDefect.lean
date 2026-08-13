c97587bb197a0d10661ad7920182000bbb082822
Jizhou Guo <mitsuha2021b@gmail.com>
2026-08-13 19:40:08 +0800

    Import spectral four-block defect theorem

--- Zeta23/StrictImprovement/SpectralZetaWiderLocalDefect.lean
+++ Zeta23/StrictImprovement/SpectralZetaWiderLocalDefect.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 SPDX-License-Identifier: Apache-2.0
 -/
 import Zeta23.StrictImprovement.SpectralCoreFour
+import Zeta23.StrictImprovement.SpectralFourDefect
 import Zeta23.StrictImprovement.ZetaBoundaryLoss
 import Zeta23.ZeroSide.Mult
 
